01a9e1c8fb2bcfd9ddd68543904f3d22b14d178b
Tikari55 <98623094+Tikari55@users.noreply.github.com>
2022-11-25 18:25:48 +0200

    Solution for first part

---
+++ Day 1/puzzle1p1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    ifstream fin("input.txt");
+
+    vector<int> data;
+
+    int element;
+    while (fin >> element)
+    {
+        data.push_back(element);
+    }
+    int count = 0;
+    int firstNum, secondNum;
+    for (int i = 0; i < data.size(); i++)
+    {
+        if (i < data.size())
+        {
+            firstNum = data[i];
+            secondNum = data[i+1];
+        }
+        if (secondNum-firstNum > 0)
+        {
+            count++;
+        }
+    }
+    cout << count << endl;
+}
